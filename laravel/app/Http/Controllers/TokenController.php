ff9278a1720e5e994714758c812b2a8e374df9fe
Black Orange <blackorange918@gmail.com>
2017-05-16 15:29:21 +0800

    log every visitor

--- laravel/app/Http/Controllers/TokenController.php
+++ laravel/app/Http/Controllers/TokenController.php
@@ -9,7 +9,7 @@ class TokenController extends BaseController
 {
 	public function ParseToken()
 	{
-		//traceHttp();
+		traceHttp();
 		$signature = Input::get('signature', 'default signature');
 		$echostr = Input::get('echostr', 'abc');
 
@@ -29,15 +29,9 @@ function traceHttp()
 
 function logger($log_content)
 {
-    if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
-        sae_set_display_errors(false);
-        sae_debug($log_content);
-        sae_set_display_errors(true);
-    }else{ //LOCAL
-        $max_size = 500000;
-        $log_filename = "log.xml";
-        print(getcwd());
-        if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
-        file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
-    }
+    $max_size = 500000;
+    $log_filename = "log.xml";
+    //print(getcwd());
+    if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
+    file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
 }
