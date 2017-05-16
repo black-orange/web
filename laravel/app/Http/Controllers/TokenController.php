e424ae2afeb8aca75267116ec073ea5148f6f2c5
Black Orange <blackorange918@gmail.com>
2017-05-16 15:05:51 +0800

    echooooooo

--- laravel/app/Http/Controllers/TokenController.php
+++ laravel/app/Http/Controllers/TokenController.php
@@ -9,14 +9,15 @@ class TokenController extends BaseController
 {
 	public function ParseToken()
 	{
-		traceHttp();
+		//traceHttp();
 		$signature = Input::get('signature', 'default signature');
 		$echostr = Input::get('echostr', 'abc');
 
 		//print($signature);
 		//print($echostr);
-		return response($echostr, 200)
-						->header('Content-Type', 'text/plain');
+		echo $echostr;
+		//return response($echostr, 200)
+		//				->header('Content-Type', 'text/plain');
 	}
 }
 
