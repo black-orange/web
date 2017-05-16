3e33152a8b806212a0e49194b5a4d3e8d4a54708
Black Orange <blackorange918@gmail.com>
2017-05-16 14:48:51 +0800

    1

--- laravel/app/Http/Controllers/TokenController.php
+++ laravel/app/Http/Controllers/TokenController.php
@@ -7,26 +7,6 @@ use Input;
 
 class TokenController extends BaseController
 {
-	public function traceHttp()
-	{
-	    logger("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
-	    logger("QUERY_STRING:".$_SERVER["QUERY_STRING"]);
-	}
-
-	public function logger($log_content)
-	{
-	    if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
-	        sae_set_display_errors(false);
-	        sae_debug($log_content);
-	        sae_set_display_errors(true);
-	    }else{ //LOCAL
-	        $max_size = 500000;
-	        $log_filename = "log.xml";
-	        if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
-	        file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
-	    }
-	}
-	
 	public function ParseToken()
 	{
 		traceHttp();
@@ -38,6 +18,24 @@ class TokenController extends BaseController
 		return response($echostr, 200)
 						->header('Content-Type', 'text/plain');
 	}
+}
 
-	
+function traceHttp()
+{
+    logger("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
+    logger("QUERY_STRING:".$_SERVER["QUERY_STRING"]);
+}
+
+function logger($log_content)
+{
+    if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
+        sae_set_display_errors(false);
+        sae_debug($log_content);
+        sae_set_display_errors(true);
+    }else{ //LOCAL
+        $max_size = 500000;
+        $log_filename = "log.xml";
+        if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
+        file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
+    }
 }
