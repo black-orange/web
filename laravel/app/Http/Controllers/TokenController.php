2765ca9c058c875b2e8ace42903abbad7caed7d6
Black Orange <blackorange918@gmail.com>
2017-05-16 14:53:31 +0800

    1

--- laravel/app/Http/Controllers/TokenController.php
+++ laravel/app/Http/Controllers/TokenController.php
@@ -35,6 +35,7 @@ function logger($log_content)
     }else{ //LOCAL
         $max_size = 500000;
         $log_filename = "log.xml";
+        print(getcwd());
         if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
         file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
     }
